2650d40fe053f8f2a23ea325c23344b32a73eee8
cavinHuang <sujinw@qq.com>
2023-09-06 08:13:36 +0800

    feat: add net

--- src-tauri/src/discovery.rs
+++ src-tauri/src/discovery.rs
@@ -16,7 +16,7 @@ use tokio::sync::oneshot;
 pub use anyhow::Result as AResult;
 use tracing::debug;
 
-use crate::utils::get_progressbar;
+// use crate::utils::get_progressbar;
 
 const SERVICE_TYPE: &str = "_rope._tcp.local.";
 
@@ -25,12 +25,26 @@ pub fn generate_magic_string() -> String {
     generator.next().unwrap()
 }
 
-pub fn register_service() {
+fn register_service(magic_string: &str, port: u16, data: HashMap<String, String>) -> AResult<()> {
   let mdns = ServiceDaemon::new().expect("Could not create service daemon");
-  let my_addrs: Vec<Ipv4Addr> = crate::utils::my_ipv4_interfaces()
-        .iter()
-        .map(|i| i.ip)
-        .collect();
+  let my_addrs: Vec<Ipv4Addr> = utils::my_ipv4_interfaces()
+      .iter()
+      .map(|i| i.ip)
+      .collect();
+  debug!("Collected addresses: {my_addrs:?}");
 
-    debug!("Collected addresses: {my_addrs:?}");
+  let service_info = ServiceInfo::new(
+    SERVICE_TYPE,
+    magic_string,
+    &format!("{magic_string}.local."),
+    &my_addrs[..],
+    port,
+    Some(data),
+  )?;
+
+  mdns.register(service_info)?;
+
+  debug!("Service registered: {magic_string}.{SERVICE_TYPE}");
+
+  Ok(())
 }
