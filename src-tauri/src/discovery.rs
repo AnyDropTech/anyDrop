7f2738aa37b2451746cb3135c15b3e009bae88a4
cavinHuang <huangchunmao1993@gmail.com>
2023-09-08 23:12:46 +0800

    feat: add plugin

--- src-tauri/src/discovery.rs
+++ src-tauri/src/discovery.rs
@@ -34,7 +34,32 @@ pub struct Person {
   color: String
 }
 
-pub fn register_service(magic_string: &str, data: HashMap<String, String>) -> AResult<()> {
+#[derive(Debug, Clone, serde::Serialize, serde::Deserialize)]
+pub struct ClientDevice {
+  nickname: String,
+  device_name: String,
+  password: String,
+  receive: bool,
+  auto_receive: bool,
+  receive_dir: String,
+  history: bool
+}
+
+impl ClientDevice {
+  fn to_hashmap(&self) -> HashMap<String, String> {
+      let mut map = HashMap::new();
+      map.insert("nickname".to_string(), self.nickname.clone());
+      map.insert("device_name".to_string(), self.device_name.clone());
+      map.insert("password".to_string(), self.password.clone());
+      map.insert("receive".to_string(), self.receive.to_string());
+      map.insert("auto_receive".to_string(), self.auto_receive.to_string());
+      map.insert("receive_dir".to_string(), self.receive_dir.clone());
+      map.insert("history".to_string(), self.history.to_string());
+      map
+  }
+}
+
+pub fn register_service(data: ClientDevice) -> AResult<()> {
   let mdns = ServiceDaemon::new().expect("Could not create service daemon");
   let my_addrs: Vec<Ipv4Addr> = utils::my_ipv4_interfaces()
       .iter()
@@ -42,18 +67,22 @@ pub fn register_service(magic_string: &str, data: HashMap<String, String>) -> AR
       .collect();
   println!("Collected addresses: {my_addrs:?}");
 
-  let instance_name = &format!("{magic_string}.local.");
+  let password = &data.password.clone();
+
+  let instance_name = &format!("{password}.local.");
+
+  let data_hash_map = data.to_hashmap();
 
   let service_info = ServiceInfo::new(
     SERVICE_TYPE,
-    magic_string,
+    password,
     instance_name,
     &my_addrs[..],
     PORT,
-    Some(data),
+    Some(data_hash_map),
   )?;
 
-  println!("Service registered: {magic_string}.{SERVICE_TYPE}");
+  println!("Service registered: {password}.{SERVICE_TYPE}");
 
   let monitor = mdns.monitor().expect("Failed to monitor the daemon");
   let service_fullname = service_info.get_fullname().to_string();
