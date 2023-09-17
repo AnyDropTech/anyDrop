c0bdac580fb05b2234b53ffc5f8b224f3a3c9146
cavinHuang <sujinw@qq.com>
2023-09-17 15:26:24 +0800

    feat: add new file

---
+++ src-tauri/src/client_command.rs
@@ -0,0 +1,17 @@
+#[derive(Debug, Clone, serde::Serialize, serde::Deserialize)]
+pub struct ClientCommand {
+
+}
+
+impl ClientCommand {
+
+  fn start_disconvery() {}
+
+  fn stop_discovery() {}
+
+  fn select_send_files() {}
+
+  fn receiver_files() {}
+
+  fn send_files() {}
+}
