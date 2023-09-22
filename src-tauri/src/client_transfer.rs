e12b49a4b62f21791fdfb6121e1ef6949f5793fd
cavinHuang <sujinw@qq.com>
2023-09-22 23:20:36 +0800

    update

--- src-tauri/src/client_transfer.rs
+++ src-tauri/src/client_transfer.rs
@@ -6,15 +6,13 @@ struct ClientTransfer {
 
 impl ClientTransfer {
 
-  pub fn send() {}
-
   pub fn send_file_info_message() {}
 
-  pub fn receiver() {}
+  pub fn send() {}
 
   pub fn receiver_file_info_message() {}
 
-  pub fn discovery_files() {}
+  pub fn receiver() {}
 }
 
 /**
